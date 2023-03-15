49f6dd3ba0aab943b2f83a919dea8516ec46b081
Drew Chase <dcmanproductions@gmail.com>
2023-03-14 22:34:59 -0400

    separated metadata to php module

---
+++ assets/php/meta.php
@@ -0,0 +1,11 @@
+
+<title><?php echo isset($title) ? $title." - " : "" ?>LFInteractive</title>
+<!-- Metadata -->
+<link rel="shortcut icon" href="/assets/img/logo.svg" type="image/x-icon">
+<meta property="og:title" content="<?php echo isset($title) ? $title." - " : "" ?>LFInteractive" />
+<meta property="og:url" content="https://lfinteractive.net<?php echo $_SERVER['PHP_SELF']; ?>" />
+<meta property="og:image" content="https://lfinteractive.net/assets/img/social-icon.jpeg" />
+<meta property="og:image:type" content="image/jpeg" />
+<meta property="og:description" content="<?php echo isset($description) ? $description : "Bring Your Brand to the Age of the Internet" ?>" />
+<meta property="og:type" content="website" />
+<meta property="fb:app_id" content="1627094801083161" />
