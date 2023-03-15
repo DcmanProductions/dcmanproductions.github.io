e62dd750125a2db7a40e4d375c367364b9bf6495
Drew Chase <dcmanproductions@gmail.com>
2023-03-14 22:35:23 -0400

    separated contact form to php module

---
+++ assets/php/contact-form.php
@@ -0,0 +1,23 @@
+<link rel="stylesheet" href="/assets/css/min/input.min.css">
+
+<section id="contact-us" class="center">
+    <div class="watermark">quote</div>
+    <div class="row">
+        <img src="/assets/img/make-a-phone-call.webp" alt="">
+        <div class="form col">
+            <h3>Request a Quote</h3>
+            <label for="name">Full Name*</label>
+            <input type="text" id="name" name="name" placeholder="John Doe" autocomplete="name" required>
+            <label for="current-website">Current Website</label>
+            <input type="text" id="current-website" name="current-website" placeholder="https://www.example.com">
+            <label for="email">Email*</label>
+            <input type="email" id="email" name="email" autocomplete="email" placeholder="john.doe@example.com">
+            <label for="phone">Phone*</label>
+            <input type="text" id="phone" name="phone" placeholder="+1 (123) 456-7890" autocomplete="mobile">
+            <label for="fname">Company / Org Name*</label>
+            <input type="text" id="fname" name="fname" placeholder="ABC Company">
+            <p class="center">( * ) required field</p>
+            <div class="btn primary">Get Quote</div>
+        </div>
+    </div>
+</section>
