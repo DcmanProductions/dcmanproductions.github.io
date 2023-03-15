c1a070141f7c67aefa166ddeb736ed2406d852b9
Drew Chase <dcmanproductions@gmail.com>
2023-03-14 22:35:08 -0400

    separated footer to php module

---
+++ assets/php/footer.php
@@ -0,0 +1,10 @@
+<link rel="stylesheet" href="/assets/css/min/footer.min.css">
+<link rel="stylesheet" href="/assets/css/min/responsive.min.css">
+
+<footer class="center">
+    lfinteractive llc. 2020-<span class="year"></span>
+</footer>
+<script src="/assets/js/min/auth.min.js"></script>
+<script src="/assets/js/min/lf.min.js"></script>
+<script src="/assets/js/min/nav.min.js"></script>
+<script src="/assets/js/min/input.min.js"></script>
