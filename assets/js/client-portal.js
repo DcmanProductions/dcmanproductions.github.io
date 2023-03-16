da61b5603232d4b761b40623fc16cffdd62143b7
Drew Chase <dcmanproductions@gmail.com>
2023-03-16 02:13:36 -0400

    fixed show password toggle

--- assets/js/client-portal.js
+++ assets/js/client-portal.js
@@ -1,5 +1,6 @@
 (() => {
+    $("input[type=password]").attr('password', true)
     $("toggle#show-password-toggle").click(e => {
-        $("input#password").attr('type', $(e.target).attr('value') != "true" ? "password" : "text")
+        $("input[password]").attr('type', $(e.target).attr('value') != "true" ? "password" : "text")
     })
 })()
