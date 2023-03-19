4d4452e1aa22d7a45f5e82fc4190564674e86023
Drew Chase <dcmanproductions@gmail.com>
2023-03-18 20:29:36 -0400

    removed page load animations

--- assets/js/lazyloading.js
+++ assets/js/lazyloading.js
@@ -3,22 +3,4 @@
         $("#landing").attr('loaded', true)
         $("nav").attr('loaded', true)
     })
-
-    let observer = new IntersectionObserver((entries, observer) => {
-        entries.forEach(entry => {
-            if (entry.isIntersecting) {
-                $(entry.target).attr('loaded', true)
-            } 
-        })
-    },
-        {
-            threshold: .5
-        })
-
-    /* Observing all the sections except the landing section. */
-    Array.from(document.querySelectorAll("section")).forEach(section => {
-        if (section.id == "landing") return;
-        observer.observe(section)
-    })
-
 })()
