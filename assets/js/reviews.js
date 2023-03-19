a27c45aac4ec5a1805b68ec52c662a0de8adee83
Drew Chase <dcmanproductions@gmail.com>
2023-03-18 20:30:25 -0400

    removed logs

--- assets/js/reviews.js
+++ assets/js/reviews.js
@@ -1,10 +1,7 @@
 (() => {
     let scrollElement = $(".reviews-slider");
-    console.log(scrollElement[0].scrollWidth)
     let slides = Math.ceil(scrollElement[0].scrollWidth / window.screen.availWidth);
-    console.log(slides)
     let speed = slides * 20;
-    console.log(speed)
     let width = scrollElement[0].scrollWidth - window.screen.availWidth + 50;
     scrollElement.css('--speed', `${speed}s`)
     scrollElement.css('--width', `-${width}px`);
