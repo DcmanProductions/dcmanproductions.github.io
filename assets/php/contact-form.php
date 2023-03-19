25aaf2773eba43b20a82a263349642118535fec5
Drew Chase <dcmanproductions@gmail.com>
2023-03-18 21:05:49 -0400

    changed names

--- assets/php/contact-form.php
+++ assets/php/contact-form.php
@@ -4,20 +4,22 @@
     <div class="watermark">quote</div>
     <div class="row">
         <img src="/assets/img/make-a-phone-call.webp" alt="">
-        <div class="form col">
+        <form class="form col">
             <h3>Request a Quote</h3>
             <label for="name">Full Name*</label>
             <input type="text" id="name" name="name" placeholder="John Doe" autocomplete="name" required>
-            <label for="current-website">Current Website</label>
-            <input type="text" id="current-website" name="current-website" placeholder="https://www.example.com">
+            <label for="website">Current Website</label>
+            <input type="text" id="website" name="website" placeholder="https://www.example.com">
             <label for="email">Email*</label>
-            <input type="email" id="email" name="email" autocomplete="email" placeholder="john.doe@example.com">
+            <input type="email" id="email" name="email" autocomplete="email" placeholder="john.doe@example.com" required>
             <label for="phone">Phone*</label>
-            <input type="text" id="phone" name="phone" placeholder="+1 (123) 456-7890" autocomplete="mobile">
-            <label for="orgname">Company / Org Name*</label>
-            <input type="text" id="orgname" name="orgname" placeholder="ABC Company" autocomplete="organization">
+            <input type="text" id="phone" name="phone" placeholder="+1 (123) 456-7890" autocomplete="mobile" required>
+            <label for="org">Company / Org Name*</label>
+            <input type="text" id="org" name="org" placeholder="ABC Company" autocomplete="organization" required>
             <p class="center">( * ) required field</p>
-            <div class="btn primary">Get Quote</div>
-        </div>
+            <div id="submit-quote" class="btn primary">Get Quote</div>
+        </form>
     </div>
 </section>
+
+<script src="/assets/js/min/quote.min.js"></script>
