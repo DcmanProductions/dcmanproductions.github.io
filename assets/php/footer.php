3020c767d32a00c64ab9bbb26ef843bc6329d484
Drew Chase <dcmanproductions@gmail.com>
2023-03-18 21:05:57 -0400

    added links

--- assets/php/footer.php
+++ assets/php/footer.php
@@ -1,8 +1,15 @@
 <link rel="stylesheet" href="/assets/css/min/footer.min.css">
 <link rel="stylesheet" href="/assets/css/min/responsive.min.css">
 
-<footer class="center">
-    lfinteractive llc. 2020-<span class="year"></span>
+<footer class="center col">
+    <span class="copy">lfinteractive llc. &copy; 2020-<?php echo date("Y"); ?></span>
+    <div class="row">
+        <a href="/">Home</a>
+        <span class="separator dot"></span>
+        <a href="/staff-portal/login">Staff Login</a>
+        <span class="separator dot"></span>
+        <a href="/staff-portal/register">Staff Registration</a>
+    </div>
 </footer>
 <script src="/assets/js/min/auth.min.js"></script>
 <script src="/assets/js/min/lf.min.js"></script>
