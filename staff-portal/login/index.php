a4d55a6a3403ca084f5186934b19c69be407c84c
Drew Chase <dcmanproductions@gmail.com>
2023-03-18 21:03:18 -0400

    added staff login page

---
+++ staff-portal/login/index.php
@@ -0,0 +1,60 @@
+<!-- A LFInteractive Project -->
+
+<!DOCTYPE html>
+<html lang="en">
+
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php
+    $title = "Staff Login";
+    include_once $_SERVER["DOCUMENT_ROOT"] . "/assets/php/meta.php";
+    ?>
+
+    <!-- Site Styles -->
+    <link rel="stylesheet" href="/assets/css/min/scrollbar.min.css">
+    <link rel="stylesheet" href="/assets/css/min/links.min.css">
+    <link rel="stylesheet" href="/assets/css/min/main.min.css">
+    <link rel="stylesheet" href="/assets/css/min/input.min.css">
+    <link rel="stylesheet" href="/assets/css/min/client-portal.min.css">
+
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="/assets/libraries/fontawesome/css/all.min.css" media="print" onload="this.media='all'">
+    <!-- jQuery -->
+    <script src="/assets/libraries/jquery.js"></script>
+</head>
+
+<body>
+
+    <?php
+    $page = -1;
+    include_once $_SERVER['DOCUMENT_ROOT'] . "/assets/php/nav.php";
+    ?>
+    <main>
+        <div class="center">
+            <div id="login-form" class="form col">
+                <h3>Staff Login</h3>
+                <label for="email">Email</label>
+                <input type="email" id="email" name="email" autocomplete="email" placeholder="john.doe@example.com">
+                <label for="password">Password</label>
+                <input type="password" id="password" name="password" autocomplete="current-password" placeholder="*************">
+                <div class="row">
+                    <toggle id="show-password-toggle">Show Password</toggle>
+                    <toggle id="remember-me-toggle">Remember Me?</toggle>
+                </div>
+                <p>New staff, <a href="/staff-portal/register/">register here</a></p>
+                <p class="error"></p>
+                <div class="btn primary">Login</div>
+            </div>
+        </div>
+    </main>
+
+    <!-- JS Scripts -->
+    <?php include_once $_SERVER["DOCUMENT_ROOT"] . "/assets/php/footer.php";    ?>
+
+    <script src="/assets/js/min/client-portal.min.js"></script>
+</body>
+
+</html>
