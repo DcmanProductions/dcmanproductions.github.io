2e2816ecf9d37f84d1aa940f815ca81a7aa2f7d5
Drew Chase <dcmanproductions@gmail.com>
2023-03-21 12:48:18 -0400

    added charts for page views and profit

--- assets/js/client-portal.js
+++ assets/js/client-portal.js
@@ -1,6 +1,67 @@
 (() => {
-    $("input[type=password]").attr('password', true)
-    $("toggle#show-password-toggle").click(e => {
-        $("input[password]").attr('type', $(e.target).attr('value') != "true" ? "password" : "text")
+    let borderWidth = 3
+    let color = "#40ffaf";
+
+    new Chart($("#page-views")[0], {
+        type: "bar",
+        maintainAspectRatio: false,
+        responsive: true,
+        data: {
+            labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+            datasets: [
+                {
+                    label: "Page Views",
+                    data: pageViews,
+                    borderWidth: borderWidth,
+                    borderColor: color,
+                    backgroundColor: color,
+                }
+            ]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            },
+            plugins: {
+                title: {
+                    display: true,
+                    text: 'Page Views'
+                }
+            }
+        }
     })
+    new Chart($("#profit")[0], {
+        type: "bar",
+        maintainAspectRatio: false,
+        responsive: true,
+        data: {
+            labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+            datasets: [
+
+                {
+                    label: "Profit",
+                    data: [0, 0, 0, 0, 0, 0, 0, 0, 2000, 86, 428, 3000],
+                    borderWidth: borderWidth,
+                    borderColor: color,
+                    backgroundColor: color,
+                }
+            ]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            },
+            plugins: {
+                title: {
+                    display: true,
+                    text: 'Website Profits'
+                }
+            }
+        }
+    })
+
 })()
