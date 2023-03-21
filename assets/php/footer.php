802c1d7c9186c7c5b0c5e96fd274658af98301ee
Drew Chase <dcmanproductions@gmail.com>
2023-03-21 12:51:47 -0400

    removed generic auth js for staff-auth and client-auth

--- assets/php/footer.php
+++ assets/php/footer.php
@@ -3,16 +3,9 @@
 
 <footer class="center col">
     <span class="copy">lfinteractive llc. &copy; 2020-<?php echo date("Y"); ?></span>
-    <div class="row">
-        <a href="/">Home</a>
-        <span class="separator dot"></span>
-        <a href="/staff-portal/login">Staff Login</a>
-        <span class="separator dot"></span>
-        <a href="/staff-portal/register">Staff Registration</a>
-    </div>
 </footer>
-<script src="/assets/js/min/auth.min.js"></script>
 <script src="/assets/js/min/lf.min.js"></script>
 <script src="/assets/js/min/nav.min.js"></script>
 <script src="/assets/js/min/input.min.js"></script>
 <script src="/assets/js/min/lazyloading.min.js"></script>
+<script src="/assets/js/min/staff-auth.min.js"></script>
